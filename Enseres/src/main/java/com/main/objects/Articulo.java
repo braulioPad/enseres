e1bdf601579b5327b013cff0bcf24156c8af03e8
Guillermo C <g.cuevas852@gmail.com>
2020-05-03 22:03:05 -0400

    add object Articulo

---
+++ Enseres/src/main/java/com/main/objects/Articulo.java
@@ -0,0 +1,73 @@
+package com.main.objects;
+
+import java.time.LocalDateTime;
+
+public class Articulo {
+
+	private String nombre;
+	private String marca;
+	private String tipo;
+	private int estado;
+	private int cantidad;
+	private LocalDateTime fechaCreacion;
+	private LocalDateTime fechaModificacion;
+	private String usuarioCreacion;
+	private String usuarioModificacion;
+	
+	
+	
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getMarca() {
+		return marca;
+	}
+	public void setMarca(String marca) {
+		this.marca = marca;
+	}
+	public String getTipo() {
+		return tipo;
+	}
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+	public int getEstado() {
+		return estado;
+	}
+	public void setEstado(int estado) {
+		this.estado = estado;
+	}
+	public int getCantidad() {
+		return cantidad;
+	}
+	public void setCantidad(int cantidad) {
+		this.cantidad = cantidad;
+	}
+	public LocalDateTime getFechaCreacion() {
+		return fechaCreacion;
+	}
+	public void setFechaCreacion(LocalDateTime fechaCreacion) {
+		this.fechaCreacion = fechaCreacion;
+	}
+	public LocalDateTime getFechaModificacion() {
+		return fechaModificacion;
+	}
+	public void setFechaModificacion(LocalDateTime fechaModificacion) {
+		this.fechaModificacion = fechaModificacion;
+	}
+	public String getUsuarioCreacion() {
+		return usuarioCreacion;
+	}
+	public void setUsuarioCreacion(String usuarioCreacion) {
+		this.usuarioCreacion = usuarioCreacion;
+	}
+	public String getUsuarioModificacion() {
+		return usuarioModificacion;
+	}
+	public void setUsuarioModificacion(String usuarioModificacion) {
+		this.usuarioModificacion = usuarioModificacion;
+	}
+}
